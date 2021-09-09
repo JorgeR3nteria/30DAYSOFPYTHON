fb172a963227f10ef6e00674ac41a7fdab0177de
JorgeR3nteria <jorge6565@live.com>
2021-09-09 02:58:13 -0500

    Se modifico y se agrego nueva información al dia 3

--- Day3_Exercise/Exercise.py
+++ Day3_Exercise/Exercise.py
@@ -4,14 +4,14 @@ from math import dist
 age=int(23)
 height=float(1.72)
 com=14+5j
-print("punto 4")
+print("Point 4")
 Base=int(input("Type a number for the base: "))
 Height=int(input("Type a number for the height: "))
+Area=0.5
 #Operations
-Total= Base*Height/2
-#Output
-print(Total)
-print("Punto 5")
+Total= Area*Base*Height
+print("Its result is:",Total)
+print("Point 5")
 #Data Input
 side_a=int(input("Type a number for side a:"))
 side_b=int(input("Type a number for side b:"))
@@ -19,8 +19,8 @@ side_c=int(input("Type a number for side c:"))
 #Operations
 perimeter=side_a+side_b+side_c
 #Output
-print(perimeter)
-print("punto 6")
+print("Its parameter is: ",perimeter)
+print("Point 6")
 #Data Input
 length=int(input("Enter the length of the rectangle: "))
 width=int(input("Enter the width of the rectangle: "))
@@ -28,22 +28,22 @@ width=int(input("Enter the width of the rectangle: "))
 area=length*width
 Perimeter=2*(length+width)
 #Output
-print("The total area of the rectangle is:",area)
-print("The perimeter of the rectangle is:",Perimeter)
-print("Punto 7")
+print("The total area of the rectangle is ",area,"and the perimeter is:",Perimeter)
+print("Point 7")
+#Data Input
 pi=3.14
 radius=float(input("Enter a number for the radius: "))
 #Result
 Area=pi*(radius**2)
 c=2*pi*radius
 #Output
-print("The area of the radius is:",Area)
-print("The cirfumference of the radius is:",c)
-print("punto 8")
+print("The area of the radius ",Area,"and the cirfumference is",c)
+'''print("Point 8")
 pending=2
 intersection_xy=2/2
-intersection_y=-2
-print("punto 9")
+intersection_y=-2'''
+print("Point 9")
+#Data Input
 #hallar la pendiente de m=y2-y1/x2-x1
 y2,y1=2,3
 x2,x1=4,2
@@ -54,7 +54,26 @@ punto_a=(2,2)
 punto_b=(6,10)
 Distancia =dist(punto_a,punto_b)
 #Output
-print("Euclidean distance is:",Distancia)
-print("The slope is: ",m)
-print("punto 21")
-hours=int(input("Type the hours "))
+print("Euclidean distance is",Distancia,"and The slope is:",m)
+#Data Input
+v1,v2= 'Python','dragon'
+print("Point 12\n""Its length is equal:",v1==v2)
+print("Point 13")
+if "on" in v1 and v2:
+    print("Yes, if found in both values")
+else:
+    print("No, the word was not found")
+print("Point 14")
+#Data
+Phrase="Espero que este curso no esté lleno de jerga"
+#Output
+if "jerga" in Phrase:
+    print("If it's in the phrase")
+else:
+    print("Not found in the phrase")
+print("Point 15\n","not found in the words 'on' python and dragon?")
+if "on" in v1 and v2:
+    print("False, if found in the 2 words")
+else:
+    print("True, the word is not found")
+print("convert the 'python' word to decimals and then to string", str(float(len("python"))))
